8250b30024ae1bcd9472b53b69b9c9959fb4acff
Galen Corey <galen.corey@gmail.com>
2017-08-01 10:16:47 -0400

    working

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 $(document).ready(function() {
-  //example code - feel free to delete!
-  var timesSillyButtonClicked = 0;
-  $('#silly-button').click(function() {
-    timesSillyButtonClicked++;
-    $('#silly-button-text').text(
-      'You clicked the silly button ' + timesSillyButtonClicked + ' times!',
-    );
-  });
-
   //write your code here!
+  $('.text-block').mouseover(function() {
+    // $(this).addClass('visible');
+    console.log('you moused over me!!!!');
+  });
 });
